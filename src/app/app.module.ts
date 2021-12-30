c3b721833e4c0011319b6a00fcfd4debb1a41ca0
Toan Nguyen <toan.1aus@gmail.com>
2021-12-31 09:19:09 +1000

    Define form controls manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product.component';
+import { ReactiveProductComponent } from './reactive-form/reactive-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    ReactiveProductComponent
   ],
   imports: [
     BrowserModule,
