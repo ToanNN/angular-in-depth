8d2739e01b914b0fb7892a3b82fffee264ec7e9e
Toan Nguyen <toan.1aus@gmail.com>
2021-12-31 08:42:55 +1000

    Validate form

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -1,4 +1,5 @@
 import { ApplicationRef, Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 import { Product } from "./model/product.model";
 import { Model } from "./model/repository.model";
@@ -27,4 +28,43 @@ export class ProductComponent {
   }
 
   selectedProduct: string | undefined;
+
+  newProduct: Product = new Product();
+  get jsonProduct() {
+    return JSON.stringify(this.newProduct);
+  }
+  addProduct(p: Product) {
+    console.log("New Product: " + this.jsonProduct);
+  }
+
+  getValidationMessages(state: any, elementName?: string) {
+    elementName = state.path || elementName;
+    let messages: string[] = [];
+    if (state.errors) {
+      for (let errorName in state.errors) {
+        switch (errorName) {
+          case "required":
+            messages.push(`You must enter a ${elementName}`);
+            break;
+          case 'minlength':
+            messages.push(`A ${elementName} must be at least ${state.errors['minlength'].requiredLength} characters`);
+            break;
+          case 'pattern':
+            messages.push(`${elementName} contains illegal characters`);
+            break;
+        }
+      }
+    }
+    return messages;
+  }
+  submittingForm: boolean = false;
+  submitForm(form: NgForm) {
+    this.submittingForm = true;
+    if (form.valid) {
+      this.addProduct(this.newProduct);
+      this.newProduct = new Product();
+      form.reset();
+      this.submittingForm = false;
+    }
+  }
 }
