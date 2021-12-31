332dddf18b616bf6aa323e88c93109c7980bcda7
Toan Nguyen <toan.1aus@gmail.com>
2021-12-31 11:28:22 +1000

    Using DOM event to raise events

--- src/app/directives/addsuccsswhitetext.directive.ts
+++ src/app/directives/addsuccsswhitetext.directive.ts
@@ -1,4 +1,6 @@
-import { Attribute, Directive, ElementRef, Input } from "@angular/core";
+import { Attribute, Directive, ElementRef, EventEmitter, Input, Output, SimpleChange } from "@angular/core";
+
+import { Product } from "../model/product.model";
 
 @Directive({
   selector: "[pa-sw]",
@@ -8,12 +10,35 @@ export class ProductAttributeSuccessWhiteText {
   @Input("pa-sw")
   hostClass: string;
 
+  @Input("pa-product")
+  product: Product | undefined;
+
+  @Output("pa-category")
+  click = new EventEmitter<string>();
   constructor(private element: ElementRef) {
     this.hostClass = "";
+    this.product = undefined;
+    this.element.nativeElement.addEventListener("click", () => {
+      if (this.product) {
+        this.click.emit(this.product.category ?? "");
+      }
+    })
   }
 
   ngOnInit() {
     this.element.nativeElement.classList.add(this.hostClass || "bg-success", "text-white")
+  }
+
+  ngOnChanges(changes: { [property: string]: SimpleChange }) {
+    let change = changes["hostClass"];
+    let classList = this.element.nativeElement.classList;
+    if (!change.isFirstChange && classList.contains(change.previousValue)) {
+      classList.remove(change.previousValue);
+    }
+
+    if (!classList.contains(change.currentValue)) {
+      classList.add(change.currentValue);
+    }
   }
 
 }
