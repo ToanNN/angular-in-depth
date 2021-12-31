4ef4ecd059d376a1a085b630f63e493dcf3fdb42
Toan Nguyen <toan.1aus@gmail.com>
2021-12-31 10:59:02 +1000

    Define binding for directive

--- src/app/directives/addsuccsswhitetext.directive.ts
+++ src/app/directives/addsuccsswhitetext.directive.ts
@@ -1,10 +1,19 @@
-import { Attribute, Directive, ElementRef } from "@angular/core";
+import { Attribute, Directive, ElementRef, Input } from "@angular/core";
 
 @Directive({
   selector: "[pa-sw]",
 })
 export class ProductAttributeSuccessWhiteText {
-  constructor(element: ElementRef, @Attribute("host-class-value") hostClass: string) {
-    element.nativeElement.classList.add(hostClass || "bg-success", "text-white");
+
+  @Input("pa-sw")
+  hostClass: string;
+
+  constructor(private element: ElementRef) {
+    this.hostClass = "";
   }
+
+  ngOnInit() {
+    this.element.nativeElement.classList.add(this.hostClass || "bg-success", "text-white")
+  }
+
 }
